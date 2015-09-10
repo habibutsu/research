fd30583f831adb46335c8ef6934d2ba0ca5f3bf6
Alexander Verbitsky <habibutsu@gmail.com>
2015-09-10 22:15:47 +0300

    Initial commit for 'lang_for_beam'

---
+++ lang_for_beam/samples/hello.erl
@@ -0,0 +1,11 @@
+-module(hello).
+
+-export([hello_world/0]).
+
+% erlc hello.erl
+% erl -noshell -s hello hello_world -s init stop
+
+hello_world() ->
+    Args = init:get_arguments(),
+    io:fwrite("hello, world\n"),
+    io:format("Init arguments: ~p~n", [Args]).
